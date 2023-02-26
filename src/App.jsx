3052a18741a6a865bdd4cb24ccc7254f3c977b15
Jason <jason.bundy@me.com>
2023-02-26 16:43:41 -0500

    added preliminary routing attempting to get indices of json href's for videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "./globals";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import Search from "./components/Search/Search";
+import SearchDetails from "./components/Search/SearchDetails";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
   // const [searchItem, setSearchItem] = useState("");
+  const [embeddedLink, setEmbeddedLink] = useState([]);
 
   const search = "search?q=";
   const searchItem = "moon";
@@ -20,11 +25,31 @@ function App() {
     getData();
   }, []);
 
+  useEffect(() => {
+    searchResults.map((result, index) => embeddedLink.push(result.href));
+  }, [searchResults, embeddedLink]);
+
   return (
     <main>
       <div className="App">
-        <Login />
-        <Home searchResults={searchResults} />
+        <Routes>
+          <Route path="/login" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route
+            path="/search"
+            element={<Search searchResults={searchResults} />}
+          ></Route>
+          <Route
+            path="/details"
+            element={
+              <SearchDetails
+                embeddedLink={embeddedLink}
+                searchResults={searchResults}
+              />
+            }
+          ></Route>
+        </Routes>
       </div>
     </main>
   );
